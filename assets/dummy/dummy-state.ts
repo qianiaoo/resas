0b848da6a9bce36c29d5627e847009c9e6a11d3a
LIU XIANG <qianiaoo@gmail.com>
2023-03-01 00:45:28 +0900

    [add]dummy data and type

---
+++ assets/dummy/dummy-state.ts
@@ -0,0 +1,20 @@
+import { CheckBoxProps } from "@/types/home";
+
+export const DUMMY_STATE_DATA: CheckBoxProps[] = [
+  {
+    isChecked: true,
+    name: "広島",
+  },
+  {
+    isChecked: true,
+    name: "島根",
+  },
+  {
+    isChecked: true,
+    name: "長崎",
+  },
+  {
+    isChecked: true,
+    name: "東京",
+  },
+];
---
+++ types/home.ts
@@ -0,0 +1,4 @@
+export type CheckBoxProps = {
+  isChecked: boolean;
+  name: string;
+};
