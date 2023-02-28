96216da8fa79a74c9732b21dd7273ceeeea64665
LIU XIANG <qianiaoo@gmail.com>
2023-03-01 00:22:23 +0900

    [add]header作成

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,21 @@
+import Head from "next/head";
 import React from "react";
+import styles from "@/styles/Home.module.css";
 
 const Header = () => {
-  return <div>Title</div>;
+  return (
+    <>
+      <Head>
+        <title>都道府県人口一覧</title>
+        <meta name="description" content="都道府県人口一覧" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.headerBox}>
+        <h3 className={styles.headerText}>都道府県人口一覧</h3>
+      </div>
+    </>
+  );
 };
 
 export default Header;
