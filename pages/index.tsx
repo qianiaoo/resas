87725a43f8ba35fe23432a0798fdf39f92d3800e
LIU XIANG <qianiaoo@gmail.com>
2023-03-01 18:42:36 +0900

    [add]人口一覧データの取得

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,48 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import CheckBoxList from "@/components/CheckBoxList";
 import Header from "@/components/Header";
+import { getPopulationByPrefCode } from "@/hooks/usePrefectures";
+import { Population, PopulationData, Prefecture } from "@/types/home";
 
 const Home: NextPage = () => {
-  const [checkedPrefCodes, setCheckedPrefCodes] = useState<number[]>([]);
-  const onCheckedChanged = (checkedState: number[]) => {
+  const [checkedPrefCodes, setCheckedPrefCodes] = useState<Prefecture[]>([]);
+  const onCheckedChanged = (checkedState: Prefecture[]) => {
     setCheckedPrefCodes(checkedState);
   };
+
+  const [populations, setPopulations] = useState<PopulationData[]>([]);
+
+  useEffect(() => {
+    let populationList: PopulationData[] = [];
+    checkedPrefCodes.map((pref) => {
+      getPopulationByPrefCode(pref.prefCode)
+        .then((res) => {
+          const apiPopulations: Population[] = res.result.data[0].data;
+          populationList.push({
+            prefName: pref.prefName,
+            populations: apiPopulations,
+          });
+
+          console.log(apiPopulations);
+        })
+        .catch((err) => {
+          console.error("ERROR, Please Check API KEY", err);
+        });
+    });
+    setPopulations(populationList);
+  }, [checkedPrefCodes]);
 
   return (
     <div className={styles.container}>
       <Header />
       <CheckBoxList onCheckedChange={onCheckedChanged} />
+      <div>
+        {populations.map((p) => (
+          <div key={p.prefName}></div>
+        ))}
+      </div>
     </div>
   );
 };
--- types/home.ts
+++ types/home.ts
@@ -2,3 +2,13 @@ export type Prefecture = {
   prefCode: number;
   prefName: string;
 };
+
+export type Population = {
+  year: number;
+  value: number;
+};
+
+export type PopulationData = {
+  prefName: string;
+  populations: Population[];
+};
